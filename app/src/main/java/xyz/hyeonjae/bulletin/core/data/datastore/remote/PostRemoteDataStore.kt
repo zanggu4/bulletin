ce3a99790ff8f32ad988737fd7cb49f906b1465c
Hyeonjae Lee <zanggu3@nate.com>
2024-07-04 21:41:37 +0900

    feat: PostRemoteDataStore.getPosts에 lastIndex 추가

--- app/src/main/java/xyz/hyeonjae/bulletin/core/data/datastore/remote/PostRemoteDataStore.kt
+++ app/src/main/java/xyz/hyeonjae/bulletin/core/data/datastore/remote/PostRemoteDataStore.kt
@@ -3,7 +3,7 @@ package xyz.hyeonjae.bulletin.core.data.datastore.remote
 import xyz.hyeonjae.bulletin.core.data.dto.PostDto
 
 interface PostRemoteDataStore {
-    suspend fun getPosts(): List<PostDto>
+    suspend fun getPosts(lastId: Int?): List<PostDto>
 
     suspend fun getPost(id: Int): PostDto?
 }
