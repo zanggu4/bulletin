8ec549914439262ef1d9991ea552c1a1ebb52b35
Hyeonjae Lee <zanggu3@nate.com>
2024-07-17 22:12:58 +0900

    feat: 게시물 상세 Api 추가

--- app/src/main/java/xyz/hyeonjae/bulletin/core/data/datasource/remote/PostApi.kt
+++ app/src/main/java/xyz/hyeonjae/bulletin/core/data/datasource/remote/PostApi.kt
@@ -2,14 +2,16 @@ package xyz.hyeonjae.bulletin.core.data.datasource.remote
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 import xyz.hyeonjae.bulletin.core.data.dto.Never
 import xyz.hyeonjae.bulletin.core.data.dto.PostDto
-import xyz.hyeonjae.bulletin.core.data.dto.PostDto.User
 import xyz.hyeonjae.bulletin.core.data.dto.ResponseDto
-
 
 interface PostApi {
     @GET("posts")
     suspend fun getPosts(@Query("lastId") lastId: Int?): Response<ResponseDto<List<PostDto>, Never>>
+
+    @GET("posts/{id}")
+    suspend fun getPost(@Path("id") id: Int): Response<ResponseDto<PostDto, Never>>
 }
---
+++ app/src/main/java/xyz/hyeonjae/bulletin/core/data/datasource/remote/di/PostRemoteDataSourceModule.kt
@@ -0,0 +1,4 @@
+package xyz.hyeonjae.bulletin.core.data.datasource.remote.di
+
+class PostApiDataSourceModule {
+}
