cc8d1947ee46a7a171216b510e772ace7b229af6
Hyeonjae Lee <zanggu3@nate.com>
2024-07-20 23:25:09 +0900

    fix: Entity 선언이 잘못된 문제 수정

--- app/src/main/java/xyz/hyeonjae/bulletin/core/data/datasource/local/AppDatabase.kt
+++ app/src/main/java/xyz/hyeonjae/bulletin/core/data/datasource/local/AppDatabase.kt
@@ -2,8 +2,9 @@ package xyz.hyeonjae.bulletin.core.data.datasource.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import xyz.hyeonjae.bulletin.core.data.dto.local.PostLocalDto
 
-@Database(entities = [PostLocalDao::class], version = 1)
+@Database(entities = [PostLocalDto::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun postDao(): PostLocalDao
 }
